00d465f35bd40cf3a0c6fb354346e78bc6e93d06
yinonozery <yinonozery@gmail.com>
2022-09-24 12:05:43 +0300

    Fix routes of express

--- routes/routes.js
+++ routes/routes.js
@@ -2,15 +2,15 @@ const express = require('express'),
     recipeRoutes = require('./recipes'),
     //ingredientRoutes = require('./ingredients'),
     categoriesRoutes = require('./categories');
-var router = express.Router();
 
+const app = express;
 // Recipes
-router.get('/data/recipe/:id', recipeRoutes.getRecipe);
-router.get('/data/recipes/', recipeRoutes.getRecipes);
-router.post('/data/recipe/', recipeRoutes.createRecipe);
+app.get('/data/recipe/:id', recipeRoutes.getRecipe);
+app.get('/data/recipes/', recipeRoutes.getRecipes);
+app.post('/data/recipe/', recipeRoutes.createRecipe);
 
 // Categories
-router.get('/data/categories/', categoriesRoutes.getCategories);
-router.get('/data/categories/:name', categoriesRoutes.getCategory);
+app.get('/data/categories/', categoriesRoutes.getCategories);
+app.get('/data/categories/:name', categoriesRoutes.getCategory);
 
 module.exports = router;
