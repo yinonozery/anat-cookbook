8f09bf4342f4c6ebaf00b61c9b35abed68559b9d
yinonozery <yinonozery@gmail.com>
2022-10-28 11:44:55 +0300

    add build

--- routes/routes.js
+++ routes/routes.js
@@ -28,7 +28,7 @@ const verifyJWT = (req, res, next) => {
         });
 };
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/build', 'index.html'));
 });
 
